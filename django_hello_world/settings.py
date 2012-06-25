1cbc21d285a6c6948f2c1ec5503a65c7a046afb3
Stanislav Matsevich <matsevich.devel@gmail.com>
2012-06-25 19:36:25 +0300

    ticket:4 done

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -92,6 +92,17 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django_hello_world.contacts.context_processors.settings",
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django_hello_world.requests.middleware.RequestsMiddleware',
