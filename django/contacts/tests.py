3f4e99a09c7f6e8886bdca691ff0ab3336985bf5
Stanislav Matsevich <matsevich.devel@gmail.com>
2012-06-29 13:13:53 +0300

    delete unnecessary tests

--- django/contacts/tests.py
+++ django/contacts/tests.py
@@ -10,14 +10,6 @@ from django.test import TestCase
 from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class HttpTest(TestCase):
     def test_contacts(self):
         c = Client()
